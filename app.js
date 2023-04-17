6a757090bf26e088b4f5ac43eea52b310c4b85d3
Myra Smith <myra.fowlkes.smith@gmail.com>
2023-04-17 11:20:51 -0400

    typo fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Budget App! For Docs, please visit...");
+  res.send("Welcome to Budget App! For Docs, please visit ...");
 });
 
 module.exports = app;
